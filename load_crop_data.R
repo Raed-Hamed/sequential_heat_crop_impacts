69e4927d61cbb51898632b8df8cd0c06abb50e22
Raed-Hamed <raed.hamed03@gmail.com>
2022-10-06 17:45:02 +0200

    added model/crop/climate code
    Concluding 2 weeks effort/ More to come soon

--- load_crop_data.R
+++ load_crop_data.R
@@ -1,4 +1,10 @@
 #======================================================================
+#clean workspace
+#======================================================================
+rm(list = ls())
+graphics.off()
+gc()
+#======================================================================
 #load libraries
 #======================================================================
 library(tidyverse)
@@ -6,80 +12,46 @@ library(sf)
 library(USAboundaries)
 library(colorspace)
 #======================================================================
-#clean workspace
+#load directories
 #======================================================================
-rm(list = ls())
-graphics.off()
-gc()
+root_dir <- getwd()
+dir_crop <- file.path(root_dir, "data/usda-nass")
+source("load_functions.R")
 #======================================================================
-#fixed parameters
+#load crop yields USDA dataframe
 #======================================================================
-#yield convert rate (from bu/acre to t/ha)
-yield_conv_rate <- 14.87 
-
-#load functions
-us_spatial_sf<-  function(x) {
-  #x is either us_counties or us_states from usaboundaries package  
-  x %>% 
-    rename(County = name,
-           State  = state_name) %>% 
-    dplyr::select(County,State,geometry) %>% 
-    mutate_at(vars(State,County), ~toupper(.))
-  
-}
+usda_wheat_df <- list.files(dir_crop, "usda-nass-wheat", full.names = TRUE) %>%
+  map_dfr(read.csv) %>%
+  dplyr::select(Year, State, County, Data.Item, Value) %>%
+  filter(Data.Item == "WHEAT, WINTER - YIELD, MEASURED IN BU / ACRE") %>%
+  mutate(Value = Value / 14.87) %>%
+  dplyr::select(-Data.Item)
 #======================================================================
-root_dir <-"C:/Users/rhd630/Desktop/PhD/Academic/paper_4"
-dir_data <-file.path(root_dir,"data/usda-nass")
-#======================================================================
-usda_wheat_df <-list.files(dir_data, "wheat", full.names = TRUE) %>%
-  map_dfr(read.csv) %>% 
-  dplyr::select(Year,State,County,Data.Item,Value) %>% 
-  filter(Data.Item == "WHEAT, WINTER - YIELD, MEASURED IN BU / ACRE") %>% 
-  mutate(Value = Value / yield_conv_rate) %>% 
-  dplyr::select(-Data.Item) 
+#transform USDA dataframe into SF object (i.e. add spatial dimension)
 #======================================================================
 usda_wheat_sf <- us_counties() %>%  us_spatial_sf %>%
-  left_join(usda_wheat_df, by = c("County","State"))%>% 
-  drop_na() %>% 
-  arrange(Year) %>% 
-  tibble() %>% 
-  group_by(State,County) %>%           
+  left_join(usda_wheat_df, by = c("County", "State")) %>%
+  drop_na() %>%
+  arrange(Year) %>%
+  tibble() %>%
+  group_by(State, County) %>%
   mutate(n = n()) %>%
-  filter(n < 44) %>% 
-  ungroup() %>% 
+  filter(n < 44) %>%
+  ungroup() %>%
   st_as_sf()
 #======================================================================
-#quick boxplot to see how many years we have per county
+#Diagnostic plot: How many years do we have per county
+#======================================================================
 usda_wheat_sf %>%
+  dplyr::select(n, geometry) %>%
+  tibble() %>%
+  distinct() %>%
+  st_as_sf() %>%
   ggplot() +
-  geom_histogram(aes(x = n), bins = 43) +
-  theme_bw(base_size = 20)
-
-#shapefile plot of years with full length timeseries
-usda_wheat_sf %>% 
-  dplyr::select(n,geometry) %>% 
-  tibble() %>% 
-  distinct() %>% 
-  st_as_sf() %>% 
-  ggplot()+
-  geom_sf(aes(fill = n))+
+  geom_sf(aes(fill = n)) +
   theme_bw(base_size = 20) +
   scale_fill_continuous_sequential("Viridis")
-
-#create usda shapefile
-usda_shp <-usda_wheat_sf %>% 
-  dplyr::select(geometry) %>% 
-  tibble() %>% 
-  distinct() %>% 
-  st_as_sf()
 #======================================================================
-usda_wheat_sf %>% 
-  dplyr::select(Year,Value) %>% 
-  filter(Year %in% 2010:2020) %>% 
-  ggplot()+
-  geom_sf(data = usda_shp, color = "lightgray", alpha = 0.5)+
-  geom_sf(aes(fill = Value))+
-  facet_wrap(~Year)+
-  scale_fill_continuous_sequential("Greens 2")+
-  theme_bw()
-
+#save crop yield data
+#======================================================================
+saveRDS(usda_wheat_sf, file.path(dir_crop,"usda_wheat_sf.rds"))
---
+++ load_functions.R
@@ -0,0 +1,266 @@
+#load functions
+us_spatial_sf<-  function(x) {
+  #x is either us_counties or us_states from usaboundaries package  
+  x %>% 
+    rename(County = name,
+           State  = state_name) %>% 
+    dplyr::select(County,State,geometry) %>% 
+    mutate_at(vars(State,County), ~toupper(.))
+  
+}
+
+#load temperature data
+load_cpc_t <-function(filename, varname, shapefile){
+  
+  list.files(dir_data, filename, full.names = TRUE) %>%
+    brick(varname = varname) %>%
+    crop(shapefile) %>%
+    mask(shapefile) %>%
+    rasterToPoints() %>%
+    data.frame() %>%
+    tibble() %>%
+    pivot_longer(X1979:X2021) %>%
+    group_by(x, y) %>%
+    mutate(Year = 1979:2021) %>%
+    dplyr::select(-name) %>%
+    ungroup()
+  
+}
+
+library(RColorBrewer)
+breaks_plot <-seq(-0.5,0.5,0.1)
+n <- breaks_plot %>%  length()
+getPalette <- colorRampPalette(brewer.pal(11, "PiYG"))
+colors_plot <- getPalette(n) #%>%  rev()
+limit <-c(-0.5,0.5)
+
+#plot coefficients at county scale
+county_scale_coefs <- function(model_coefs_df, model_pvals_df, sf_mask){
+  
+  ggplot() +
+    geom_sf(data = sf_mask, fill = "darkgrey") +
+    geom_sf(data = model_coefs_df,
+            aes(fill = estimate),
+            color = "transparent") +
+    geom_sf(data = model_pvals_df, size = 0.1) +
+    facet_wrap( ~ term) +
+    geom_sf(
+      data = model_coefs_df %>%
+        dplyr::select(geometry) %>%
+        tibble() %>%
+        distinct() %>%
+        st_as_sf(),
+      fill = "transparent",
+      size = 0.0001,
+      color = "transparent",
+      alpha = 0.5
+    ) +
+    scale_fill_gradient2(
+      name = "β coefficient",
+      midpoint = 0,
+      low = "firebrick4",
+      mid = "white",
+      high = "forestgreen",
+      breaks = seq(-0.8, 0.8, 0.1),
+      limits = c(-0.8,  0.8)
+    ) +
+    # scale_fill_stepsn(
+    #   name = "β coefficient",
+    #   colours = colors_plot,
+    #   breaks = breaks_plot,
+    #   limit = limit,
+    #   labels = function(x) {
+    #     x <- as.character(x %>%  round(1))
+    #     x[!1:0] <- " "
+    #     x
+    #   }
+    # ) +
+  geom_sf(data = sf_mask, fill = "transparent") +
+    theme_bw(base_size = 15) +
+    theme(
+      legend.text = element_text(size = 13),
+      legend.key.width = unit(1, "cm"),
+      legend.position = "bottom",
+      legend.spacing.x = unit(0, 'cm')
+    ) +
+    guides(fill = guide_legend(
+      label.position = "bottom",
+      nrow = 1,
+      override.aes = list(size = 1.5)
+    ))
+  
+  
+}
+  
+  
+  
+
+#plot dependence plot based on averaging county scale coefficients per state
+plot_dependence_per_state <-function(model_coefs,observation_df,state_name,pval_thrsh){
+  
+  # state_name <- "MONTANA"
+  # pval_thrsh <- 0.1
+  
+  sign_counties <-model_coefs %>% 
+    filter(p.value <= pval_thrsh) %>% 
+    tibble() %>% 
+    .$County
+  
+  #Get coefficients for model with interactions for a specific state
+  coefs_df <- model_coefs %>% 
+    filter(p.value <= pval_thrsh) %>% 
+    dplyr::select(County, State, term, estimate) %>%
+    group_by(term) %>%
+    filter(State == state_name) %>% 
+    tibble() %>% 
+    group_by(term) %>% 
+    summarise_at(vars(estimate),~mean(., na.rm = TRUE))
+  
+  #Get coefficients with confidence bounds
+  coefs_df_confidence_interval <- model_coefs%>%
+    tibble() %>% 
+    filter(p.value <= pval_thrsh) %>% 
+    dplyr::select(County, State, term, estimate, conf.low, conf.high) %>%
+    group_by(term) %>%
+    filter(State == state_name) %>% 
+    summarize_at(vars(-County, -State),  ~ (mean(., na.rm = TRUE)))
+  
+  #get points montana
+  obs_df <-observation_df %>% 
+    ungroup() %>% 
+    filter(County %in% sign_counties) %>% 
+    dplyr::select(-geometry,-group_id)%>% 
+    na.exclude() %>% 
+    filter(State == state_name) %>%
+    unique()
+  
+  #model space predictions
+  model_space_predictions <-
+    expand.grid(kdd_spring = seq(-4,4,0.05),
+                kdd_summer = seq(-4,4,0.05)) %>%
+    mutate(
+      crop_yield =
+        pull(coefs_df[1, 2]) * kdd_spring +
+        pull(coefs_df[2, 2]) * kdd_summer + 
+        pull(coefs_df[3, 2]) * kdd_spring * kdd_summer 
+    ) %>%
+    unique()
+  
+  #interaction plot
+  interaction_df <-
+    expand.grid(
+      kdd_summer = obs_df$kdd_summer,
+      kdd_spring = c(
+        obs_df$kdd_spring %>%  quantile(0.05) %>%  round(1),
+        obs_df$kdd_spring %>%  quantile(0.5) %>%  round(1),
+        obs_df$kdd_spring %>%  quantile(0.95) %>%  round(1)
+      )
+    ) %>%
+    mutate(
+      crop_yield =
+        pull(coefs_df_confidence_interval[1, 2]) * kdd_spring +
+        pull(coefs_df_confidence_interval[2, 2]) * kdd_summer + 
+        pull(coefs_df_confidence_interval[3, 2]) * kdd_spring * kdd_summer ,
+      
+      crop_yield.low =
+        pull(coefs_df_confidence_interval[1, 3]) * kdd_spring +
+        pull(coefs_df_confidence_interval[2, 3]) * kdd_summer + 
+        pull(coefs_df_confidence_interval[3, 3]) * kdd_spring * kdd_summer ,
+      
+      crop_yield.high =
+        pull(coefs_df_confidence_interval[1, 4]) * kdd_spring +
+        pull(coefs_df_confidence_interval[2, 4]) * kdd_summer + 
+        pull(coefs_df_confidence_interval[3, 4]) * kdd_spring * kdd_summer ,
+    ) %>% 
+    unique() %>% 
+    as.data.frame() %>%   
+    tibble() %>% 
+    mutate(kdd_spring = factor(paste(
+      kdd_spring)))
+  
+  #Change factor names
+  levels(interaction_df$kdd_spring) <- c("percentile_5th",
+                                         "percentile_50th",
+                                         "percentile_95th")
+  dependence_plot <-
+    #define x,y,z vars for contour lines
+    ggplot(data  = model_space_predictions,
+           aes(x = kdd_spring,
+               y = kdd_summer,
+               z = crop_yield)) +
+    #add contour lines
+    geom_contour(aes(colour = ..level..),
+                 size = 2,
+                 show.legend = FALSE) +
+    #add points
+    geom_point(
+      data = obs_df,
+      aes(x = kdd_spring, y = kdd_summer, fill = crop_yield),
+      color = "black",
+      size = 4,
+      shape = 21,
+      inherit.aes = FALSE,
+      alpha = 1
+    ) +
+    #add title
+    ggtitle(label = state_name)+
+    #add axes
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 0) +
+    #adjust general plot features
+    xlab(expression("Standardized spring KDD")) +
+    ylab(expression("Standardized summer KDD")) +
+    scale_color_continuous_diverging(palette = "Red-Green") +
+    scale_fill_continuous_diverging(name = "", palette = "Red-Green") +
+    theme_classic(base_size = 20)
+  #======================================================================
+  interaction_plot <-
+    ggplot(interaction_df ,
+           aes(
+             y = crop_yield,
+             x = kdd_summer,
+             color = kdd_spring,
+             linetype = kdd_spring
+           )) +
+    #add line
+    geom_line(size = 2) +
+    #add CI
+    geom_ribbon(
+      data = interaction_df,
+      aes(
+        ymin = crop_yield.low,
+        ymax = crop_yield.high,
+        fill = kdd_spring
+      ),
+      linetype = 0,
+      alpha = 0.2,
+      show.legend = FALSE
+    ) +
+    #Adjust legend
+    scale_color_manual("Spring KDD",
+                       values = c("#bc7124", "#999999", "#209281"),
+                       labels = c("Extreme cool (5%)", "Normal (50%)", "Extreme warm (95%)")
+    ) +
+    #Adjust linetype
+    scale_linetype_manual(values = c("dashed", "solid" , "dashed")) +
+    guides(linetype = FALSE) +
+    #adjust CI
+    scale_fill_manual(values = c("#bc7124", "#999999", "#209281"),
+                      labels = c("")) +
+    #adjust general plot features
+    ylab("Standardized Yield Anomaly") +
+    xlab("Standardized summer KDD") +
+    theme_classic(base_size = 20) +
+    theme(
+      legend.position = c(0.40, 0.20),
+      legend.background = element_rect(fill = alpha("white", 0))
+    )+
+    xlim(-2, 2)
+  
+  library(patchwork)
+  conmbined_plot <- dependence_plot +
+    interaction_plot +
+    plot_annotation(tag_levels = list(c("(a)", "(b)")))
+  
+  return(conmbined_plot)
+}
