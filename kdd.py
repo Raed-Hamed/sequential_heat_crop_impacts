7ffc3d3fd076db5d6e096e94123aa4307c5ba28c
Carmen Steinmann <carmenst@student.ethz.ch>
2022-11-08 13:17:40 +0100

    Add testing of soil moisture to climate data preprocessesing.

--- kdd.py
+++ kdd.py
@@ -269,60 +269,15 @@ def plot_gdd_kdd(year, lat, lon, tmax, tmin, harvest_end_mean, thr_season, dates
     plt.savefig(filename, format='pdf', bbox_inches='tight')
 
 
+def plot_map(variable, lat, lon, var_name= 't max', title='T max global'):
+    lon1, lat1 = np.meshgrid(lon, lat)
+    coords = np.append(lat1.reshape(360*720,1), lon1.reshape(360*720,1), axis=1)
 
-"""TESTING"""
-# test_kdd = sum_kdd_summer[6,107,130]
-# year = 6
-# lat = 107
-# lon = 130
-# harvest_grid_cell = harvest_end_mean[lat, lon]
-# day_summer_start = int(harvest_grid_cell-summer_start)
-# day_summer_end = int(harvest_grid_cell-summer_end)
-# kdd_summer_gridcell = tmax[day_summer_start:day_summer_end, lat, lon, :] - t_high_summer
-# kdd_summer_gridcell[kdd_summer_gridcell<=0] = 0
-# #kdd_summer_gridcell[np.isnan(kdd_summer_gridcell)] = 0
-# size_kdd = kdd_summer_gridcell.shape
-# total_kdd = np.sum(kdd_summer_gridcell, axis=0)
-
-# #testing gdd
-# np.max(gdd_spring[~np.isnan(gdd_spring)]) / gdd_max_spring
-# np.max(gdd_summer[~np.isnan(gdd_summer)]) / gdd_max_summer
-# max_gdd = np.max(gdd_summer[~np.isnan(gdd_summer)])
-# median_gdd = np.median(gdd_summer[~np.isnan(gdd_summer)])
-# #grid_cells = np.where(gdd_summer == max_gdd)
-# grid_cells = np.where(gdd_summer >= median_gdd) and np.where(sum_kdd_summer>=20)
-# year = 0
-# lat = 82
-# lon = 140
-# # year = 0
-# # lat = 47
-# # lon = 66
-# harvest_grid_cell = harvest_end_mean[lat, lon]
-# day_spring_start = int(harvest_grid_cell-spring_start)
-# day_spring_end = int(harvest_grid_cell-spring_end)
-# day_summer_start = int(harvest_grid_cell-summer_start)
-# day_summer_end = int(harvest_grid_cell-summer_end)
-# tmax_gridcell = tmax[day_spring_start:day_spring_end, lat, lon, :]
-# tmin_gridcell = tmin[day_spring_start:day_spring_end, lat, lon, :]
-# gdd_spring_gridcell = (tmax[day_spring_start:day_spring_end, lat, lon, :] + 
-#                 tmin[day_spring_start:day_spring_end, lat, lon, :])/2 - t_base_spring
-# gdd_spring_gridcell = (tmax_gridcell + tmin_gridcell)/2 - t_base_spring
-# gdd_spring_gridcell[gdd_spring_gridcell<=0] = 0
-# gdd_spring_gridcell[gdd_spring_gridcell>=gdd_max_spring] = gdd_max_spring
-# gdd_spring[:, lat, lon] = np.sum(gdd_spring_gridcell, axis=0)
-
-# gdd_summer_gridcell = (tmax[day_summer_start:day_summer_end, lat, lon, :] + 
-#                 tmin[day_summer_start:day_summer_end, lat, lon, :])/2 - t_base_summer
-# gdd_summer_gridcell[gdd_summer_gridcell<=0] = 0
-# gdd_summer_gridcell[gdd_summer_gridcell>=gdd_max_summer] = gdd_max_summer
-# gdd_summer[:, lat, lon] = np.sum(gdd_summer_gridcell, axis=0)
-
-# kdd_max_year = tmax_gridcell[:, int(year)]
-# gdd_spring_year = gdd_spring_gridcell[:, int(year)]
+    u_plot.geo_im_from_array(variable.reshape(360*720,1), coords, 
+                             var_name, title)
 
 
-
-"""PLOTTING ONLY KDD"""
+#"""PLOTTING ONLY KDD"""
 # year_max, lat_max, lon_max = np.where(sum_kdd_summer == np.max(sum_kdd_summer[~np.isnan(sum_kdd_summer)]))
 # harvest_grid_cell = harvest_end_mean[int(lat_max), int(lon_max)]
 # day_summer_start = int(harvest_grid_cell-summer_start)
@@ -339,14 +294,3 @@ def plot_gdd_kdd(year, lat, lon, tmax, tmin, harvest_end_mean, thr_season, dates
 # plt.legend(loc='upper right')
 # plt.xlim(day_summer_start, day_summer_end-1)
 # plt.savefig('kdd_vis.png', format='png', bbox_inches='tight')
-
-
-
-def plot_map(variable, lat, lon, var_name= 't max', title='T max global'):
-    lon1, lat1 = np.meshgrid(lon, lat)
-    coords = np.append(lat1.reshape(360*720,1), lon1.reshape(360*720,1), axis=1)
-
-    u_plot.geo_im_from_array(variable.reshape(360*720,1), coords, 
-                             var_name, title)
-
-
