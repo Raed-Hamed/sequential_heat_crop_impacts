99cd77c6684d3a0fc71045bf27eda1da10e5f0c2
Carmen Steinmann <carmenst@student.ethz.ch>
2024-05-27 17:21:45 +0200

    Add jupyter notebook for pre-processing of European data

--- climate_preprocessing/historic_metric.py
+++ climate_preprocessing/historic_metric.py
@@ -2,8 +2,24 @@ import numpy as np
 import xarray as xr
 from pathlib import Path
 
+SEASONS = ['spring', 'summer']
 
-def monthly_t_metrics(path, lon_bounds, lat_bounds, months, thresholds):
+CROPS_DICT = {'wheat': {'spring_season': [3,4],
+                        'spring_month': 4,
+                        'threshold_spring': 15,
+                        'summer_season': [5,6],
+                        'summer_month': 6,
+                        'threshold_summer': 20.5},
+              'maize': {'spring_season': [5,6],
+                        'spring_month': 6,
+                        'threshold_spring': 28.8,
+                        'summer_season': [7,8],
+                        'summer_month': 8,
+                        'threshold_summer': 30.5}
+              }
+
+
+def monthly_t_metrics(path, lon_bounds, lat_bounds, months, thresholds, years):
     files = [f.name for f in path.iterdir() if f.is_file() and not f.name.startswith('.')]
     files.sort()
 
@@ -17,11 +33,11 @@ def monthly_t_metrics(path, lon_bounds, lat_bounds, months, thresholds):
     results_ds = xr.concat(datasets, dim='time')
     
     output = results_ds.rename_dims({'time': 'year'})
-    output['year'] = np.arange(1980, 2022)
+    output['year'] = np.arange(years[0], years[1])
     
     return output
 
-def compute_t_metrics(file, lon_bounds, lat_bounds, months, thresholds):
+def compute_t_metrics(file, lon_bounds, lat_bounds, months, thresholds, variable='tmax'):
     
     ds  = xr.open_dataset(file)
     
@@ -44,9 +60,7 @@ def compute_t_metrics(file, lon_bounds, lat_bounds, months, thresholds):
     
     # Compute mean per month
     monthly_mean = months_selection.groupby('time.month').mean(dim='time')
-    output_ds = monthly_mean.rename_vars({'tmax': 'monthly_mean'})
-    
-    
+    output_ds = monthly_mean.rename_vars({variable: 'monthly_mean'})
 
     for threshold in thresholds: 
         """Sum above threshold"""
@@ -54,24 +68,24 @@ def compute_t_metrics(file, lon_bounds, lat_bounds, months, thresholds):
         values_above_threshold = (months_selection - threshold).clip(min=0).fillna(0)
         # Sum values above threshold per month
         values_above_threshold_per_month = values_above_threshold.groupby('time.month').sum(dim='time')
-        output_ds['values_above_thr_'+str(threshold)] = values_above_threshold_per_month.tmax
+        output_ds['values_above_thr_'+str(threshold)] = values_above_threshold_per_month[variable]
         
         """Days above threshold"""
         # Create a boolean mask indicating values above the threshold
         above_threshold_mask = months_selection > threshold
         # Count the number of days per month that exceed the threshold
         days_above_threshold_per_month = above_threshold_mask.groupby('time.month').sum(dim='time')
-        output_ds['days_above_thr_'+str(threshold)] = days_above_threshold_per_month.tmax
+        output_ds['days_above_thr_'+str(threshold)] = days_above_threshold_per_month[variable]
     
     
     return output_ds
 
-def annual_t_metrics(t_monthly, crops, seasons, metrics, crops_dict):
+def annual_t_metrics(t_monthly, crops, metrics):
     sum_metric = []
     name_metric = []
     for crop in crops:
-        crop_var = crops_dict[crop]
-        for season in seasons:
+        crop_var = CROPS_DICT[crop]
+        for season in SEASONS:
             for metric in metrics:
                 metric_values = t_monthly[metric+str(crop_var[f'threshold_{season}'])]
                 sum_metric.append(metric_values.sel(month=slice(*crop_var[f'{season}_season'])).groupby('year').sum('month'))
@@ -103,29 +117,30 @@ def annual_t_metrics(t_monthly, crops, seasons, metrics, crops_dict):
     return final
 
 
-def sm_metrics(path_moisture, lon_bounds, lat_bounds, crops, seasons, crops_dict):
+def sm_metrics(path_moisture, lon_bounds, lat_bounds, crops, variable='SMroot'):
     
     ds_moisture = xr.open_dataset(path_moisture)
     
     study_area = ds_moisture.sel(lon=slice(lon_bounds[0], lon_bounds[1]), 
-                            lat=slice(lat_bounds[0], lat_bounds[1])) 
+                                 lat=slice(lat_bounds[0], lat_bounds[1])) 
 
     sum_metric = []
     name_metric = []
     for crop in crops:
-        crop_var = crops_dict[crop]
-        for season in seasons:
+        crop_var = CROPS_DICT[crop]
+        for season in SEASONS:
             #monthly mean across months
             months = crop_var[f'{season}_season']
             months_selection = study_area.sel(time=study_area['time.month'].isin(months))
-            sum_metric.append(months_selection.groupby('time.year').mean('time')['SMroot'])
-            name_metric.append(crop+'_'+season+'_'+'monthly_mean_'+'season')
+            sum_metric.append(months_selection.groupby('time.year').mean('time')[variable])
+            name_metric.append(crop+'_'+season)
+            #REST OF THE NAME: +'_'+'monthly_mean_'+'season'
             
-            #monthly mean per month
-            months = crop_var[f'{season}_month']
-            months_selection = study_area.sel(time=study_area['time.month'].isin(months))
-            sum_metric.append(months_selection.groupby('time.year').mean('time')['SMroot'])
-            name_metric.append(crop+'_'+season+'_'+'monthly_mean_'+'month')
+            # #monthly mean per month
+            # months = crop_var[f'{season}_month']
+            # months_selection = study_area.sel(time=study_area['time.month'].isin(months))
+            # sum_metric.append(months_selection.groupby('time.year').mean('time')[variable])
+            # name_metric.append(crop+'_'+season+'_'+'monthly_mean_'+'month')
             
     # Create an empty dataset
     final_dataset = xr.Dataset()
@@ -134,3 +149,6 @@ def sm_metrics(path_moisture, lon_bounds, lat_bounds, crops, seasons, crops_dict
         final_dataset[name] = metric
     
     return final_dataset
+
+
+
