ba41cd6402f05f4a7c4951538ed1b6835d348546
Carmen Steinmann <carmenst@student.ethz.ch>
2024-05-27 13:28:15 +0200

    Add computation of mean per model for cmip6 computations

--- climate_preprocessing/cmip6_means.py
+++ climate_preprocessing/cmip6_means.py
@@ -279,7 +279,8 @@ def compute_frequency_change(path_input):
         
         
         new_file = file.replace("exceedances", "frequency")
-        new_file1 = new_file.replace("spring/sp_", "coincide_")
+        # new_file1 = new_file.replace("spring/sp_", "coincide_")
+        new_file1 = new_file.replace("spring/wtsp_", "coincide_")
         
         dict_var = dict(frequency=(["lat", "lon"], nr_coincides))
         create_ds_2D(ds_spring.lat.values, ds_spring.lon.values, dict_var, new_file1)
@@ -386,6 +387,64 @@ def compute_f_mean(path):
     
     crops_shapefile.to_file(new_file2)
     
+def compute_f_mean_per_model(path):
+        
+    us_shp_file = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/input/gadm36_USA_2.shp'
+    crops_shapefile = gpd.read_file(us_shp_file)
+    
+    files_his = [f for f in iglob(path+'/*', recursive=True) if os.path.isfile(f)]
+    multi_models = np.zeros((crops_shapefile.shape[0], len(files_his)))
+    
+    
+    for idx, file in enumerate(files_his):
+        ds = xr.open_dataset(file)
+        # ds['f_change'].values[ds['f_change'].values>=100] = 100000
+        ds['f_change'].values[ds['f_change'].values>=100] = np.nan
+        
+        _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, reference, percentile, f_increase, ssp,filename = file.split('/')
+        _, _, _, model, _, _, nr_percen = filename.split('_')
+        
+        mean_counties = fct_mean_per_county(ds, model, 'f_change', crops_shapefile)
+        
+        crops_shapefile['M_'+str(idx)] = mean_counties
+        
+        multi_models[:, idx] = mean_counties  
+
+    new_file1 = file.replace(reference+'/'+percentile+'/'+f_increase+'/'+ssp,'/result')
+    new_file2 = new_file1.replace(filename, 'f_change_relative_'+ssp+'_'+reference+'_'+percentile+'.shp')
+    
+    # idx_unprece = np.where(multi_models == 100000)
+    # mask_unprece = np.zeros(multi_models.shape)
+    # mask_unprece[idx_unprece] = 1
+    # perc_unprece = (np.sum(mask_unprece, axis=1))/len(files_his)
+    
+    increase_agreement = np.zeros(multi_models.shape)
+    increase_agreement[np.where(multi_models >0)] = 1
+    
+    # remove nan values
+    # multi_models[idx_unprece] = np.nan
+    
+    # multi model mean (disregarding nan values) and percentage of models predicting an increase in co-occurence
+    crops_shapefile['mean_incre'] = np.nanmean(multi_models, axis=1)
+    crops_shapefile['%M_incre'] = np.sum(increase_agreement, axis=1)/len(files_his)
+    
+    # percentage of models agreeing in amplitude of frequency change
+    oom_of_mean = np.zeros(multi_models.shape)
+    std = np.nanstd(multi_models, axis=1)
+    larger_than_min = np.where(multi_models >(crops_shapefile['mean_incre'].values-std)[:,None])
+    smaller_than_max = np.where(multi_models <(crops_shapefile['mean_incre'].values+std)[:,None])
+    oom_of_mean[larger_than_min and smaller_than_max] = 1
+    percen_m_increase = (np.sum(oom_of_mean, axis=1))/len(files_his)
+    crops_shapefile['%M_ampli'] = percen_m_increase
+    
+    # save percentage of models predicting unprecedented hot-hot extremes
+    # crops_shapefile['%M_unpre'] = perc_unprece
+    
+    crops_shapefile.to_file(new_file2)
+
+
+
+
 
 def fct_mean_per_county(ds, model, var_name, crops_shapefile):
     ds_out = regrid_cmip6(ds, model, var_name)
@@ -454,40 +513,44 @@ def create_ds_2D(lat, lon, dict_var, file_path):
 
 """Compute mean per time period and grid cell for historic and future time period"""
 # # future
-# path_future = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/future/input/**/*' 
+# # path_future = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/future/input/**/*' 
+# path_future = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/future/input/**/*' 
 # nr_years = 40
 # create_mean(path_future, nr_years, "2015-2100")
 
 
 # # historic
 # path_historic = '/Users/carmenst/Documents/CLIMADA/own_projects/sequential_heat_crop_impacts/CMIP6_DATA/historic/input/**/*' 
-# # '/Users/carmenst/Documents/CLIMADA/own_projects/sequential_heat_crop_impacts/CMIP6_DATA/output'
+# path_historic = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/historic/input/**/*' 
 
 # create_mean(path_historic, nr_years, "1961-2014")
 
 
 # # """Compute multi model mean"""
-# path_future_output = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/future/output/**/*' 
+# # path_future_output = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6//future/output/**/*' 
+# path_future_output = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/future/output/**/*' 
 # compute_difference(path_future_output)
 
 
 """Compute value for each county"""
-path_difference = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/future-historic/output/**/*' 
-# # # crops_shapefile = compute_mean_per_county(path_difference)
-# # # crops_shapefile.to_file('counties_multi_model_mean.shp')
-crops_shapefile = compute_diff_per_model(path_difference)
-crops_shapefile.to_file('counties_difference_per_model.shp')
+# path_difference = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/future-historic/output/**/*' 
+# # # # crops_shapefile = compute_mean_per_county(path_difference)
+# # # # crops_shapefile.to_file('counties_multi_model_mean.shp')
+# crops_shapefile = compute_diff_per_model(path_difference)
+# crops_shapefile.to_file('counties_difference_per_model.shp')
 
-#compute exceedances of thr
-path_historic = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/historic/input/**/*' 
+# #compute exceedances of thr
+# path_historic = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/historic/input/**/*' 
+path_historic = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/historic/input/**/*' 
 nr_years = 40
 percentiles = [50, 75]
 references = ['historic', 'future']
-#ssps= ['ssp245', 'ssp370']
-ssps= ['ssp119', 'ssp126']
+# #ssps= ['ssp245', 'ssp370']
+# ssps= ['ssp119', 'ssp126']
+ssps= ['ssp119', 'ssp126', 'ssp245', 'ssp370']
 
-us_shp_file = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/input/gadm36_USA_2.shp'
-shp_US = gpd.read_file(us_shp_file)
+# us_shp_file = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/input/gadm36_USA_2.shp'
+# shp_US = gpd.read_file(us_shp_file)
 
 
 for percentile in percentiles: 
@@ -495,38 +558,39 @@ for percentile in percentiles:
     for reference in references:
         reference_str = '/reference_'+reference
         
-        # frequency
-        compute_frequency(path_historic, nr_years, percentile, reference, ssps)
+        # # frequency
+        # compute_frequency(path_historic, nr_years, percentile, reference, ssps)
         
         # frequency change
-        path_scenario = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/frequencies' +(
+        path_scenario = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/frequencies' +(
             reference_str+ percentile_str +'/exceedances')
         compute_frequency_change(path_scenario)
         
         for ssp in ssps:
             # occurence of hot-hot events in the past and future
-            path_cooccurrence = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/frequencies' + (
+            path_cooccurrence = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/frequencies' + (
                 reference_str+ percentile_str +'/frequency')
             comparison_fut_his(path_cooccurrence, ssp)
             
             # compare changes in co-occurence
-            path = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/frequencies'+(
+            path = '/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/Climate/CMIP6/wheat/frequencies/'+(
                 reference_str+percentile_str +'/frequency increase/'+ssp)
-            compute_f_mean(path)
+            # compute_f_mean(path)
+            compute_f_mean_per_model(path)
 
 # import numpy as np
 # import geopandas as gpd
 # import matplotlib.pyplot as plt
 # import cartopy.crs as ccrs
 # import copy
-# # us_shp_file ='/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/output/2023_06_T_diff/counties_difference_per_model.shp'
+# # # us_shp_file ='/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/output/2023_06_13_T_diff/counties_difference_per_model.shp'
 
-# scenario = 'f_change_relative_ssp370_reference_historic_percentile_50.shp'
-# us_shp_file ='/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/output/2023_06_f_change/'+scenario
+# scenario = 'f_change_relative_ssp119_reference_future_percentile_50.shp'
+# us_shp_file ='/Users/carmenst/Documents/Polybox/WCR/Conferences_summerschools/2022_10_Como/Project/Sequential_heat_crops/Data/shapefiles/output/2023_08_18_f_chage_per_model/'+scenario
 # shp_US = gpd.read_file(us_shp_file)
  
 
-# shp_US.plot(column='%M_incre', cmap ='seismic' ,legend=True, vmin=0, vmax=1)
+# shp_US.plot(column='mean_incre', cmap ='seismic' ,legend=True, vmin=-10, vmax=10)
 
 # mean = shp_US['mean_incre'].values
 # perc_increase = shp_US['%M_incre'].values
